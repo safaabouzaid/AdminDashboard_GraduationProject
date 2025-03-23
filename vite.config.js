910928ae76278adf9e2c3d4707e9517852e66336
safaabouzaid <abouzaidsafa@gmail.com>
2025-03-24 01:23:07 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ["react-icons"]
+    }
+  }
 });
