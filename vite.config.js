ca27da3d76e9bdca572f2a2ae0808fd8e0e27e91
safaabouzaid <abouzaidsafa@gmail.com>
2025-04-19 20:50:44 +0300

    fix/netlify

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ['@mui/material', '@mui/system'],
-    }
-  },
   optimizeDeps: {
     include: ['@mui/material', '@mui/system', '@mui/icons-material'],
   },  
-
-  chunkSizeWarningLimit: 1000 ,
-  
+  chunkSizeWarningLimit: 1000,
   commonjsOptions: {
-    include: [/node_modules/]
+    include: [/node_modules/],
   },
   resolve: {
     alias: {
