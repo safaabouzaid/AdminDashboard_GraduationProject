39a95f505476348b703c3b87506675fa8c98fcc7
safaabouzaid <abouzaidsafa@gmail.com>
2025-06-18 11:07:54 +0300

    .

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: 'https://ee00-185-132-133-96.ngrok-free.app'
+    API_BASE_URL: 'https://7407-185-132-133-96.ngrok-free.app'
   };
   
   export default config;
