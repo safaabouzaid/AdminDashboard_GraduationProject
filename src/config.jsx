973debfe2c54ec4549c6efd026c3fb3033365d49
safaabouzaid <abouzaidsafa@gmail.com>
2025-06-24 23:20:57 +0300

    fix/BASE_URL

--- src/config.jsx
+++ src/config.jsx
@@ -5,7 +5,7 @@
 const isLocal = window.location.hostname === 'localhost';
 
 const config = {
-  API_BASE_URL: isLocal ? 'http://127.0.0.1:8000/' : 'https://5e5b-169-150-196-103.ngrok-free.app/',
+  API_BASE_URL: isLocal ? 'http://127.0.0.1:8000/' : 'https://backend-forsatech.onrender.com/',
 
   CLOUDINARY_UPLOAD_PRESET: 'forsa-_unsigned_upload', 
   CLOUDINARY_CLOUD_NAME: 'doerwhivd',
